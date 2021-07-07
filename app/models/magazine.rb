8a9a12b15d9a0eb1f9025b9530bd571e3beb23ae
Grant Cloyd <dgcloyd@gmail.com>
2021-07-07 15:45:30 -0500

    commit tree d10b57831763904ca15ef22fda5cc7832a2df80c

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,14 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions  
+
+  def email_list 
+    self.readers.map {|r| r.email}.join(';')
+  end
+
+  def self.most_popular 
+    filtered =  Magazine.all.map {|m| m.subscriptions }.select {|a,b| a <=> b}.flatten.first
+    Magazine.find(filtered.magazine_id)
+  end
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,17 @@
 class Reader < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+  def subscribe (magazine, price)
+     Subscription.create(magazine_id: magazine.id, price: price, reader_id: self.id )
+  end
   
+  def total_subscription_price 
+    self.subscriptions.map {|i| i.price}. reduce {|a,b| a +b}
+  end
+   
+  def cancel_subscription(magazine) 
+    self.subscriptions.find_by(magazine_id: magazine.id).destroy
+  end
+
 end
